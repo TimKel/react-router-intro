0cf7efe7be35b66ed19e2c353e030fbef19f8087
Tim Kelley <timkelley683@gmail.com>
2022-10-01 13:18:56 -0700

    chocolate component

---
+++ src/Chocolate.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+const Chocolate = () => {
+    console.log("HELLO")
+    return (
+            <div>
+                <h1>Chocolate</h1>
+                <img
+                    src="https://i.pinimg.com/736x/2b/1c/ae/2b1caecc723712711329d543026f8277--candy-bar-labels-candy-bar-wrappers.jpg"
+                    alt="Willy Wonka Bar"
+                />
+                <h2><Link to="/">Home</Link></h2>
+            </div>
+    )
+}
+
+export default Chocolate;
